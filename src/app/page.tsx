2c361a8369df4f280dcbcc65a389916038af69f5
matutaj <matutajorge2002@gmail.com>
2023-07-31 19:35:58 +0100

    feat: add next auth providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from 'next/image'
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+
+  const { data } = useSession();
+
   return (
-    <div> Home</div>
+    <div>
+      <button onClick={() => signIn()}>Login</button>
+      <button onClick={() => signOut()}>LogOut</button>
+
+
+      <h1>Olá, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ""} alt="" />
+    </div>
   )
 }
