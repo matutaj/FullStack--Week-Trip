1bf5246e7d56f1b6c5c0aed79c769dbbf1b771d9
matutaj <matutajorge2002@gmail.com>
2023-08-10 18:18:10 +0100

    feat:add input carreny controller

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,11 +1,28 @@
+import DatePicker from "@/components/DatePicker";
+import Input from "@/components/Input";
 import React from "react";
+import CurrencyInput from "react-currency-input-field";
 
 const TripSearch = () => {
     return (
         <div className="container mx-auto p-5">
             <h1 className="font-semibold text-2xl  text-center">
                 Encontre sua próxima <span className="text-primary">Viagem!</span> </h1>
+
+            <div className="felx flex-col gap-4 mt-5">
+                <Input placeholder="Onde Você quer Ir!" />
+
+                <div className="flex gap-4">
+                    <DatePicker placeholderText="Data da IDa " onChange={() => { }} />
+                    <CurrencyInput placeholder="Orçamento" />
+
+                </div>
+            </div>
+
+
         </div>
+
+
     )
 }
 
