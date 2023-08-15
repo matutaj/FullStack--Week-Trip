e1e2cf4178a2c5a20fe9c1a195a0355116f33cc0
matutaj <matutajorge2002@gmail.com>
2023-08-15 12:32:13 +0100

    feat: add initial search trip

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -6,8 +6,8 @@ import CurrencyInput from "react-currency-input-field";
 
 const TripSearch = () => {
     return (
-        <div className="container mx-auto p-5">
-            <h1 className="font-semibold text-2xl  text-center">
+        <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
+            <h1 className="font-semibold text-2xl text-primaryDarker text-center">
                 Encontre sua próxima <span className="text-primary">Viagem!</span> </h1>
 
             <div className="felx flex-col gap-4 mt-5">
