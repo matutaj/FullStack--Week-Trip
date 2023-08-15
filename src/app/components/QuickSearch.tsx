bff56f8cdfa817db7ac2e78ed1d932e9e9bd0a6e
matutaj <matutajorge2002@gmail.com>
2023-08-15 14:12:39 +0100

    feat: add quick

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -30,11 +30,7 @@ const QuickSearch = () => {
 
             </div>
 
-            <div className="flex items-center mt-4">
-                <div className="w-full h-[1px] bg-grayLighter" ></div>
-                <h2 className=" px-5 font-medium text-grayPrimary whitespace-nowrap"> Destinos Rescomendados</h2>
-                <div className="w-full h-[1px] bg-grayLighter" ></div>
-            </div>
+
         </div>
     )
 }
