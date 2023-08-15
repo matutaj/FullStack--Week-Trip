e92604578376cdf6f3f859cf03baa3ec2de66d26
matutaj <matutajorge2002@gmail.com>
2023-08-15 13:08:49 +0100

    feat:add QuickSearch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import QuickSearch from "./components/QuickSearch"
 import TripSearch from "./components/TripSearch"
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
       <TripSearch />
+      <QuickSearch />
     </div>
   )
 }
