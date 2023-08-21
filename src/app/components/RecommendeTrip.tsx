8404559237caa986cf3dc1f004ea005df6f44452
matutaj <matutajorge2002@gmail.com>
2023-08-21 14:28:04 +0100

    feat: add trip item compoment in recommended trip

--- src/app/components/RecommendeTrip.tsx
+++ src/app/components/RecommendeTrip.tsx
@@ -1,4 +1,6 @@
 
+import TripItem from "@/components/TripItem";
+import { Trip } from "@prisma/client";
 import React from "react";
 
 const RecommendedTrip = async () => {
@@ -10,7 +12,13 @@ const RecommendedTrip = async () => {
                 <h2 className=" px-5 font-medium text-grayPrimary whitespace-nowrap"> Destinos Rescomendados</h2>
                 <div className="w-full h-[1px] bg-grayLighter" ></div>
             </div>
-
+            <div className="flex flex-col items-center mt-5 gap-5">
+                {
+                    data.map((trip: Trip) => (
+                        <TripItem key={trip.id} trip={trip} />
+                    ))
+                }
+            </div>
         </div>
     )
 }
