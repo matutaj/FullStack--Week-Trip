02d370613cda492de55b5f4c18cb1588da484ee2
matutaj <matutajorge2002@gmail.com>
2023-09-04 13:02:05 +0100

    feat: add logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="bg-walterWhite p-5 justify-center flex flex-col items-center">
-            <Image src="/logo.png" width={133} height={23} alt="Full Stack Week" />
+            <Image src="/Logo.png" width={133} height={23} alt="Full Stack Week" />
             <p className="text-sm mt-1 font-medium text-primaryDarker">Todos os direitos Reservados.</p>
         </div>
     )
