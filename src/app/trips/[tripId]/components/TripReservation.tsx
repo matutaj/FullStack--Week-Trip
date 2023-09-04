40e24314aa590dc5a898b927fb973a57db906f0e
matutaj <matutajorge2002@gmail.com>
2023-09-04 13:01:36 +0100

    feat: add validation form with react-hook-form

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -5,27 +5,83 @@ import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { Trip } from "@prisma/client";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
     trip: Trip
 }
 
+interface TripReservationForm {
+    guests: number
+    startDate: Date | null
+    endDate: Date | null
+}
+
 const TripReservation = ({ trip }: TripReservationProps) => {
+    const { register, handleSubmit, formState: { errors }, control } = useForm<TripReservationForm>();
+
+    const onSubmit = (data: any) => {
+        console.log({ data })
+    }
     return (
         <div>
             <div className="flex flex-col px-5 ">
                 <div className="flex gap-3">
-                    <DatePicker placeholderText="Data de Início" onChange={() => { }} className="w-full" />
-                    <DatePicker placeholderText="Data final" onChange={() => { }} className="w-full" />
+                    <Controller
+                        name="startDate"
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "Data Inicial é obrigatória",
+                            }
+                        }
+                        }
+                        control={control}
+                        render={({ field }) => (<DatePicker
+                            error={!!errors?.startDate}
+                            errorMessage={errors?.startDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data de Início"
+                            className="w-full" />
+                        )}
+                    />
+                    <Controller
+                        name="endDate"
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "Data Final é obrigatória",
+                            }
+                        }
+                        }
+                        control={control}
+                        render={({ field }) => (<DatePicker
+                            error={!!errors?.endDate}
+                            errorMessage={errors?.endDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data Final"
+                            className="w-full" />
+                        )}
+                    />
                 </div>
-                <Input placeholder={`Número de hóspedes max(${trip.maxGuests})`} className="mt-4" />
+                <Input {...register("guests", {
+                    required: {
+                        value: true,
+                        message: "Número de Hóspodes é obrigatório. "
+                    }
+                })}
+                    error={!!errors?.guests}
+                    errorMessage={errors?.startDate?.message}
+                    placeholder={`Número de hóspedes max(${trip.maxGuests})`} className="mt-4" />
                 <div className="flex justify-between mt-3">
                     <p className="font-medium texte-sm text-primaryDarker">Total:</p>
                     <p className="font-medium texte-sm text-primaryDarker">R$2500</p>
 
                 </div>
                 <div className="pb-10 border-b border-grayLighter w-full    ">
-                    <Button className="w-full mt-2">Reservar Agora</Button>
+                    <Button onClick={() => handleSubmit(onSubmit)()} className="w-full mt-2">Reservar Agora</Button>
 
                 </div>
             </div>
