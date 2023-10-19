f57522902b8b801f2575b719ac44bfa4fefea7d8
matutaj <matutajorge2002@gmail.com>
2023-10-20 00:18:58 +0100

    fix: solve the bug on prisma

--- src/lib/prisma.tsx
+++ src/lib/prisma.tsx
@@ -2,7 +2,36 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
+export const { prisma } = globalForPrisma || new PrismaClient({
     log: ["query"],
 });
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma
+
+
+
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//     namespace NodeJS {
+//         interface Global {
+//             prisma: PrismaClient;
+//         }
+//     }
+// }
+
+// let prisma: PrismaClient;
+
+// if (typeof window === "undefined") {
+//     if (process.env.NODE_ENV === "production") {
+//         prisma = new PrismaClient();
+//         // } else {
+//         //     if (!global.prisma) {
+//         //         global.prisma = new PrismaClient();
+//         //     }
+
+//         //     prisma = global.prisma;
+
+//     }
+// }
+
+// export { prisma } 
